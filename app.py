aa8c7473f15f3ffee80189f1cbba82f9aa64a341
Yves Macedo <yves.macedo@gmail.com>
2022-01-02 17:33:03 -0300

    Arquivos da Janela

---
+++ app.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+ARQUIVO = "README.md"
+APPDATA = os.path.expanduser("~") + "\\AppData\\Local\\DomGarca"
+
+if not os.path.exists(APPDATA):
+	os.makedirs(APPDATA)
+
+f = open(APPDATA + "\\.dir","w")
+f.write(os.path.join(os.path.dirname(os.path.abspath(ARQUIVO)),""))
+f.close()
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(ARQUIVO)),"lib"))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(ARQUIVO)),"mvc"))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(ARQUIVO)),"app"))
+
+from servidor import Server
+
+if __name__=="__main__":
+	Server()
--- domgarca.py
+++ domgarca.py
@@ -1,22 +1,7 @@
-# -*- coding: utf-8 -*-
-import os
-import sys
+from cefpython3 import cefpython
 
-ARQUIVO = "README.md"
-APPDATA = os.path.expanduser("~") + "\\AppData\\Local\\DomGarca"
-
-if not os.path.exists(APPDATA):
-	os.makedirs(APPDATA)
-
-f = open(APPDATA + "\\.dir","w")
-f.write(os.path.join(os.path.dirname(os.path.abspath(ARQUIVO)),""))
-f.close()
-
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(ARQUIVO)),"lib"))
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(ARQUIVO)),"mvc"))
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(ARQUIVO)),"app"))
-
-from servidor import Server
-
-if __name__=="__main__":
-	Server()
+if __name__ == '__main__':
+    cefpython.Initialize()
+    cefpython.CreateBrowserSync(url = "https://www.google.com/", window_title = "Dom Garça")
+    cefpython.MessageLoop()
+    cefpython.Shutdown()
